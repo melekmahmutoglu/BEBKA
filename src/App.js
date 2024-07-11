c0edb50144f661b488165dc9d6df1b7ea5bb33ea
Melek Mahmutoğlu <melekmahmutoglu.555@gmail.com>
2024-07-11 11:57:53 +0300

    some changes done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BEBKA EGİTİM DENEMELERİ
         </p>
         <a
           className="App-link"
